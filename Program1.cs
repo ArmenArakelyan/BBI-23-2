e000417bdb3bfdb61ee4092df3f32c4314c1cd2d
ArmenArakelyan <161484880+ArmenArakelyan@users.noreply.github.com>
2024-04-02 00:04:38 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    { ////#1 
+        using System;
+        using System.Collections.Generic;
+        using System.Linq;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Run_500m> run500 = new List<Run_500m>();
+
+            run500.Add(new Run_500m("Иванова", "Группа 1", "Тренер Ларкова", 120));
+            run500.Add(new Run_500m("Петрова", "Группа 2", "Тренер Паркова", 110));
+            run500.Add(new Run_500m("Сидорова", "Группа 1", "Тренер Жаркова", 100));
+            run500.Add(new Run_500m("Смирнова", "Группа 3", "Тренер Маркова", 90));
+
+            Console.WriteLine("Результаты кросса на 500м:");
+            Console.WriteLine("Фамилия\t\t Группа \t\t Преподаватель \t\t Результат в секундах \t\t Норматив");
+
+            foreach (var runner in run500.OrderBy(runner => runner.Result))
+            {
+                runner.DisplayInfo();
+            }
+
+            int passedNorm = run500.Count(runner => runner.Result <= 100);
+            Console.WriteLine($"\nОбщее количество участниц, выполнивших норматив: {passedNorm}");
+
+            List<Run_100m> run100 = new List<Run_100m>();
+
+            run100.Add(new Run_100m("Иванова", "Группа 1", "Тренер Ларкова", 11));
+            run100.Add(new Run_100m("Петрова", "Группа 2", "Тренер Паркова", 7));
+            run100.Add(new Run_100m("Сидорова", "Группа 1", "Тренер Жаркова", 13));
+            run100.Add(new Run_100m("Смирнова", "Группа 3", "Тренер Маркова", 8));
+
+            Console.WriteLine("Результаты кросса на 100м:");
+            Console.WriteLine("Фамилия\t\t Группа \t\t Преподаватель \t\t Результат в секундах \t\t Норматив");
+
+            foreach (var runner in run100.OrderBy(runner => runner.Result))
+            {
+                runner.DisplayInfo();
+            }
+
+            passedNorm = run100.Count(runner => runner.Result <= 100);
+            Console.WriteLine($"\nОбщее количество участниц, выполнивших норматив: {passedNorm}");
+        }
+    }
+
+    abstract class Runner
+    {
+        public string Surname { get; private set; }
+        public string Group { get; private set; }
+        public string TeacherSurname { get; private set; }
+        public double Result { get; private set; }
+
+        public Runner(string surname, string group, string teacherSurname, double result)
+        {
+            Surname = surname;
+            Group = group;
+            TeacherSurname = teacherSurname;
+            Result = result;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"{Surname}\t\t{Group}\t\t{TeacherSurname}\t\t{Result}");
+        }
+    }
+
+    class Run_500m : Runner
+    {
+        public Run_500m(string Surname, string Group, string TeacherSurname, double Result) : base(Surname, Group, TeacherSurname, Result)
+        {
+
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"{Surname}\t\t{Group}\t\t{TeacherSurname}\t\t{Result:F2}\t\t{(Result <= 100 ? "Выполнен" : "Не выполнен")}");
+        }
+    }
+
+    class Run_100m : Runner
+    {
+        public Run_100m(string Surname, string Group, string TeacherSurname, double Result) : base(Surname, Group, TeacherSurname, Result)
+        {
+
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"{Surname}\t\t{Group}\t\t{TeacherSurname}\t\t{Result:F2}\t\t{(Result <= 10 ? "Выполнен" : "Не выполнен")}");
+        }
+    }
+
+    List<Run_500m> run500 = new List<Run_500m>();
+
+    run500.Add(new Run_500m("Иванова", "Группа 1", "Тренер Ларкова", 120)); 
+        run500.Add(new Run_500m("Петрова", "Группа 2", "Тренер Паркова", 110)); 
+        run500.Add(new Run_500m("Сидорова", "Группа 1", "Тренер Жаркова", 100)); 
+        run500.Add(new Run_500m("Смирнова", "Группа 3", "Тренер Маркова", 90)); 
+
+        Console.WriteLine("Результаты кросса на 500м:"); 
+        Console.WriteLine("Фамилия\t\t Группа \t\t Преподаватель \t\t Результат в секундах \t\t Норматив"); 
+
+        foreach (var runner in run500.OrderBy(runner => runner.Result)) 
+        { 
+            runner.DisplayInfo(); 
+        }
+
+int passedNorm = run500.Count(runner => runner.Result <= 100);
+Console.WriteLine($"\nОбщее количество участниц, выполнивших норматив: {passedNorm}");
+
+List<Run_100m> run100 = new List<Run_100m>();
+
+run100.Add(new Run_100m("Иванова", "Группа 1", "Тренер Ларкова", 11));
+run100.Add(new Run_100m("Петрова", "Группа 2", "Тренер Паркова", 7));
+run100.Add(new Run_100m("Сидорова", "Группа 1", "Тренер Жаркова", 13));
+run100.Add(new Run_100m("Смирнова", "Группа 3", "Тренер Маркова", 8));
+
+Console.WriteLine("Результаты кросса на 100м:");
+Console.WriteLine("Фамилия\t\t Группа \t\t Преподаватель \t\t Результат в секундах \t\t Норматив");
+
+foreach (var runner in run100.OrderBy(runner => runner.Result))
+{
+    runner.DisplayInfo();
+}
+
+passedNorm = run100.Count(runner => runner.Result <= 100);
+Console.WriteLine($"\nОбщее количество участниц, выполнивших норматив: {passedNorm}"); 
+    } 
+} 
+
+abstract class Runner
+{
+    public string Surname { get; private set; }
+    public string Group { get; private set; }
+    public string TeacherSurname { get; private set; }
+    public double Result { get; private set; }
+
+    public Runner(string surname, string group, string teacherSurname, double result)
+    {
+        Surname = surname;
+        Group = group;
+        TeacherSurname = teacherSurname;
+        Result = result;
+    }
+
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"{Surname}\t\t{Group}\t\t{TeacherSurname}\t\t{Result}");
+    }
+}
+
+class Run_500m : Runner
+{
+    public Run_500m(string Surname, string Group, string TeacherSurname, double Result) : base(Surname, Group, TeacherSurname, Result)
+    {
+
+    }
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"{Surname}\t\t{Group}\t\t{TeacherSurname}\t\t{Result:F2}\t\t{(Result <= 100 ? "Выполнен" : "Не выполнен")}");
+    }
+}
+
+class Run_100m : Runner
+{
+    public Run_100m(string Surname, string Group, string TeacherSurname, double Result) : base(Surname, Group, TeacherSurname, Result)
+    {
+
+    }
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"{Surname}\t\t{Group}\t\t{TeacherSurname}\t\t{Result:F2}\t\t{(Result <= 10 ? "Выполнен" : "Не выполнен")}");
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+class Person
+{
+    private string family;
+    public string Family { get { return family; } }
+
+
+    public Person(string family1)
+    {
+        family = family1;
+
+    }
+}
+
+class Student : Person
+{
+    private static int id;
+    private int number;
+    private double rez1, rez2, rez3, rez4;
+    private double rez;
+    private double sr(double x, double y, double z, double w)
+    {
+        return (x + y + z + w) / 4;
+    }
+    public double Rez { get { return rez; } }
+    public Student(string family, double rezz1, double rezz2, double rezz3, double rezz4) : base(family)
+    {
+        rez = 0;
+        rez1 = rezz1;
+        rez2 = rezz2;
+        rez3 = rezz3;
+        rez4 = rezz4;
+        rez = sr(rez1, rez2, rez3, rez4);
+        number = Convert.ToInt32($"2305{id}");
+        id++;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Фамилия {0}\t Номер студ билета {1}\t Средний балл {2,4:f2}", Family, number, Rez);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student[] os = new Student[4];
+        os[0] = new Student("Умкин", 3.0, 4.0, 5.0, 4.0);
+        os[1] = new Student("Пупкин", 4.0, 5.0, 3.0, 5.0);
+        os[2] = new Student("Лупкин", 3.0, 3.0, 4.0, 5.0);
+        os[3] = new Student("Зупкин", 5.0, 5.0, 5.0, 5.0);
+
+        foreach (var student in os)
+        {
+            student.DisplayInfo();
+        }
+
+        for (int i = 0; i < os.Length - 1; i++)
+        {
+            double amax = os[i].Rez;
+            int imax = i;
+            for (int j = i + 1; j < os.Length; j++)
+            {
+                if (os[j].Rez > amax)
+                {
+                    amax = os[j].Rez;
+                    imax = j;
+                }
+            }
+            Student temp;
+            temp = os[imax];
+            os[imax] = os[i];
+            os[i] = temp;
+        }
+
+        Console.WriteLine();
+
+        foreach (var student in os)
+        {
+            if (student.Rez >= 4)
+            {
+                student.DisplayInfo();
+            }
+        }
+    }
+}
